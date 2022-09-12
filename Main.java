8ec984386d37f4f6932ec51d102f09f9306202c9
Milan Labus <107197194+MilanLabuss@users.noreply.github.com>
2022-09-12 16:14:15 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,31 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	
+	public static String url = ("/application/Login.fxml");
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			
+			Parent root = FXMLLoader.load(getClass().getResource(url));
+			Scene scene = new Scene(root,737,547);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
